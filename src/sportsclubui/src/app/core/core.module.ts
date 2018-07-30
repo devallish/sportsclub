6189cf3f1367c9773f25940858731ed94515f453
Dave Gregory <devallish@googlemail.com>
2018-07-30 19:27:08 +0100

    added font and font awesome and style layout

---
+++ src/sportsclubui/src/app/core/core.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainNavigationComponent } from './ui/main-navigation/main-navigation.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MainNavigationComponent]
+})
+export class CoreModule { }
